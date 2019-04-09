4dc2d2d764f0186772fb02924c3deb5ac3f1a419
Aaron O'Mullan <aaron.omullan@gmail.com>
2019-04-10 00:38:46 +0200

    Implement "quoi" an express-like routing layer for cloudflare workers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import quoi from './quoi';
 import FirebaseOnCloudflare from './firebase';
 import { ReverseProxy, CachedProxy, StaticEndpoint, StaticHeaders } from './reverseproxy';
 
 export default FirebaseOnCloudflare;
 export {
+    quoi,
     ReverseProxy,
     CachedProxy,
     StaticEndpoint,
