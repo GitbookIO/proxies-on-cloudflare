863b42e6346b50c857dd0b74e81bc2be0f39412c
Johan Preynat <johan.preynat@gmail.com>
2021-08-18 01:28:49 +0200

    Move jest config to file and setup environment

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    // testEnvironment: 'jsdom',
+    setupFiles: [
+        './jest.setup.js'
+    ]
+};
